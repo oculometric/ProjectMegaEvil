a297b3446452bf4f9b0dd3a91e0d9c5b66741eed
dumpling5462 <maxgillham2@gmail.com>
2025-03-04 22:41:04 +0000

    created data assets for the fish and boat

---
+++ Source/ProjectMegaEvil/ProjectMegaEvil.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectMegaEvil.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectMegaEvil, "ProjectMegaEvil" );
---
+++ Source/ProjectMegaEvil/Public/DA_BaseBoat.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_BaseBoat.generated.h"
+
+
+UCLASS()
+class PROJECTMEGAEVIL_API UDA_BaseBoat : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMesh> _BoatMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float _BoatMaxHealth;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float _BoatSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float _BoatStrength;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float _BoatCost;
+	
+};
